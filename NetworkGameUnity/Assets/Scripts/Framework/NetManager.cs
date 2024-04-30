d1c9faee4536834bb04d9415bedd197f80b71bc4
BlueSky <lsxk0830@163.com>
2024-04-30 22:05:02 +0800

    Connect

--- NetworkGameUnity/Assets/Scripts/Framework/NetManager.cs
+++ NetworkGameUnity/Assets/Scripts/Framework/NetManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Net.Sockets;
+using UnityEngine;
 
 public static class NetManager
 {
@@ -8,6 +10,8 @@ public static class NetManager
     private static Queue<ByteArray> writeQueue; // 写入队列
     public delegate void EventListener(string err); // 事件委托类型
     private static Dictionary<NetEvent, EventListener> eventListeners = new Dictionary<NetEvent, EventListener>(); // 事件监听列表
+
+    private static bool isConnecting = false; // 是否正在连接
 
     #region 事件监听、移除、分发
     /// <summary>
@@ -45,8 +49,71 @@ public static class NetManager
     /// <param name="err">传给回调方法的字符串</param>
     private static void FireEvent(NetEvent netEvent, string err)
     {
-        if(eventListeners.ContainsKey(netEvent))
+        if (eventListeners.ContainsKey(netEvent))
             eventListeners[netEvent](err);
     }
     #endregion 事件监听、移除、分发
+
+    /// <summary>
+    /// 连接
+    /// </summary>
+    /// <param name="ip"></param>
+    /// <param name="port"></param>
+    public static void Connect(string ip, int port)
+    {
+        if (socket != null && socket.Connected)
+        {
+            Debug.Log("Connect Fail,already connected!");
+            return;
+        }
+
+        if (isConnecting)
+        {
+            Debug.Log("Connect Fail,isConnecting!");
+            return;
+        }
+
+        // 初始化成员
+        InitState();
+        // 参数设置
+        socket.NoDelay = true;
+        // Connect
+        isConnecting = true;
+        socket.BeginConnect(ip, port, ConnectCallback, socket);
+    }
+
+    /// <summary>
+    /// 初始化成员
+    /// </summary>
+    private static void InitState()
+    {
+        // socket
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        // 接收缓冲区
+        readBuff = new ByteArray();
+        // 写入队列
+        writeQueue = new Queue<ByteArray>();
+        // 是否正在连接
+        isConnecting = false;
+    }
+
+    #region Socket回调
+    private static void ConnectCallback(IAsyncResult ar)
+    {
+        try
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            socket.EndConnect(ar);
+            Debug.Log("Socket Connect Succ");
+            FireEvent(NetEvent.ConnectSucc, "");
+            isConnecting = false;
+        }
+        catch (SocketException ex)
+        {
+            Debug.LogError($"Socket Connect fail {ex.ToString()}");
+            FireEvent(NetEvent.ConnectFail, ex.ToString());
+            isConnecting = false;
+        }
+    }
+    #endregion Socket回调
 }
