58516e16b84285c3204d51a4b59ee95ab592b06c
BlueSky <lsxk0830@163.com>
2024-05-01 15:41:15 +0800

    协议体的编码解码

--- NetworkGameUnity/Assets/Scripts/Framework/MsgBase.cs
+++ NetworkGameUnity/Assets/Scripts/Framework/MsgBase.cs
@@ -6,10 +6,36 @@ M s g M o v e：【协议名】长度由“协议名长度”确定
 { " x " = 1 } : 【协议体】可由它解析出MsgMove对象
 
 */
+using System;
+using UnityEngine;
+
 public class MsgBase
 {
     /// <summary>
     /// 协议名
     /// </summary>
     public string protoName = "";
+
+    /// <summary>
+    /// 编码
+    /// </summary>
+    public static byte[] Encode(MsgBase msgBase)
+    {
+        string s = JsonUtility.ToJson(msgBase);
+        return System.Text.Encoding.UTF8.GetBytes(s);
+    }
+
+    /// <summary>
+    /// 解码
+    /// </summary>
+    /// <param name="protoName">协议名</param>
+    /// <param name="bytes">要解码的byte数组</param>
+    /// <param name="offset">byte数组开始位置</param>
+    /// <param name="count">byte数组从开始位置要解析的字节数</param>
+    public static MsgBase Decode(string protoName,byte[] bytes,int offset,int count)
+    {
+        string s = System.Text.Encoding.UTF8.GetString(bytes,offset,count);
+        MsgBase  msgBase = (MsgBase)JsonUtility.FromJson(s,Type.GetType(protoName));
+        return msgBase;
+    }
 }
