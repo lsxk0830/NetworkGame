4113d27a64e4d9b63477274d40f4e4cc5548364c
蓝色星空 <lsxk0830@163.com>
2024-10-22 19:40:01 +0800

    优化调整

--- Game/Scripts/proto/SyncMsg.cs
+++ Game/Scripts/proto/SyncMsg.cs
@@ -1,4 +1,22 @@
-// 同步坦克信息、开火、击中
+// 同步协议、同步坦克信息、开火、击中
+
+/// <summary>
+/// 同步协议
+/// </summary>
+public class MsgFrameSync : MsgBase
+{
+    public MsgFrameSync() { protoName = "MsgFrameSync"; }
+
+    /// <summary>
+    /// 指令，0-前进 1-后退 2-左转 3-右转 4-停止
+    /// </summary>
+    public int cmd = 0;
+
+    /// <summary>
+    /// 在第几帧发生事件
+    /// </summary>
+    public int frame = 0;
+}
 
 /// <summary>
 /// 同步坦克信息
