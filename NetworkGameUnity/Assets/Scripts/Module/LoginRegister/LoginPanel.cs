bd5d6f8f24842802667db4e693682f0ebfb49c4c
蓝色星空 <lsxk0830@163.com>
2024-11-22 17:51:56 +0800

    增加记住密码

--- NetworkGameUnity/Assets/Scripts/Module/LoginRegister/LoginPanel.cs
+++ NetworkGameUnity/Assets/Scripts/Module/LoginRegister/LoginPanel.cs
@@ -1,4 +1,4 @@
-using System;
+using SimpleFrame;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,7 +9,10 @@ public class LoginPanel : BasePanel
     public TMP_InputField pwInput; // 密码输入框
     public Button loginBtn; // 登录按钮
     public Button registerBtn; // 注册按钮
+    public Button readPwBtn; // 本地读取密码按钮
     public Toggle isShowPwToggle; // 是否显示密码
+    public Toggle RememberPwToggle; // 是否记住密码
+    private GameObject tipPanel; // 自动输入密码弹窗
 
     public override void OnInit() // 初始化
     {
@@ -24,11 +27,16 @@ public class LoginPanel : BasePanel
         pwInput = skin.transform.Find("PWInput").GetComponent<TMP_InputField>();
         loginBtn = skin.transform.Find("LoginBtn").GetComponent<Button>();
         registerBtn = skin.transform.Find("RegisterBtn").GetComponent<Button>();
-        isShowPwToggle = skin.transform.Find("IsShowPwToggle").GetComponent<Toggle>();
+        tipPanel = skin.transform.Find("TipPanel").gameObject;
+        readPwBtn = skin.transform.Find("TipPanel/ReadPwBtn").GetComponent<Button>();
+        isShowPwToggle = skin.transform.Find("PWInput/IsShowPwToggle").GetComponent<Toggle>();
+        RememberPwToggle = skin.transform.Find("RememberPwToggle").GetComponent<Toggle>();
         // 监听
         loginBtn.onClick.AddListener(OnLoginClick);
         registerBtn.onClick.AddListener(onRegisterClick);
         isShowPwToggle.onValueChanged.AddListener(OnPwShowChangeClick);
+        readPwBtn.onClick.AddListener(OnReadPwClick);
+        idInput.onEndEdit.AddListener(IdInputEnd); // 用户名输入名结束
 
         // 网络协议监听
         NetManager.AddMsgListener("MsgLogin", OnMsgLogin);
@@ -37,6 +45,21 @@ public class LoginPanel : BasePanel
         NetManager.AddEventListener(NetManager.NetEvent.ConnectFail, OnConnectFail);
         // 连接服务器
         NetManager.Connect("127.0.0.1", 8888);
+
+        RememberPwToggle.isOn = PlayerPrefs.GetInt("RememberPwToggle") == 0 ? true : false;
+        if (RememberPwToggle.isOn)
+        {
+            string str = PlayerPrefs.GetString("idPw");
+            int index = str.IndexOf(',');
+            if (index != -1)
+            {
+                // 获取分隔符前的部分
+                idInput.text = str.Substring(0, index);
+                // 获取分隔符后的部分
+                pwInput.text = str.Substring(index + 1);
+                this.Log($"id:{idInput.text},pw:{pwInput.text}");
+            }
+        }
     }
 
     public override void OnClose() // 关闭
@@ -55,7 +78,6 @@ public class LoginPanel : BasePanel
             PanelManager.Open<TipPanel>("用户名和密码不能为空");
             return;
         }
-
         // 发送
         MsgLogin msgLogin = new MsgLogin()
         {
@@ -76,6 +98,20 @@ public class LoginPanel : BasePanel
         pwInput.ForceLabelUpdate();
     }
 
+    private void IdInputEnd(string arg0)
+    {
+        if (PlayerPrefs.GetString(idInput.text) == "")
+            return;
+        tipPanel.SetActive(true);
+    }
+
+    private void OnReadPwClick()
+    {
+        string pw = PlayerPrefs.GetString(idInput.text);
+        pwInput.text = pw;
+        tipPanel.SetActive(false);
+    }
+
     #endregion
 
     /// <summary>
@@ -86,6 +122,11 @@ public class LoginPanel : BasePanel
         MsgLogin msg = (MsgLogin)msgBse;
         if (msg.result == 0)
         {
+            PlayerPrefs.SetInt("RememberPwToggle", RememberPwToggle.isOn ? 0 : 1); // 是否保存密码
+            if (RememberPwToggle.isOn)
+            {
+                PlayerPrefs.SetString("idPw", $"{idInput.text},{pwInput.text}");
+            }
             Debug.Log($"收到OnMsgLogin协议:登录成功");
             GameMain.id = msg.id;
             PanelManager.Open<RoomListPanel>();
@@ -99,10 +140,7 @@ public class LoginPanel : BasePanel
     /// <summary>
     /// 连接成功回调
     /// </summary>
-    private void OnConnectSucc(string err)
-    {
-        Debug.Log($"OnConnectSucc");
-    }
+    private void OnConnectSucc(string err) { }
 
     /// <summary>
     /// 连接失败回调
@@ -111,7 +149,7 @@ public class LoginPanel : BasePanel
     {
         GloablMono.Instance.TriggerFromOtherThread(() =>
         {
-            PanelManager.Open<TipPanel>(err,2);
+            PanelManager.Open<TipPanel>(err);
         });
     }
 }
