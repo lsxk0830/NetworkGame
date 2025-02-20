7b9734e413b8ea8ab12aa66c891931a881cd7d7a
蓝色星空 <lsxk0830@163.com>
2025-02-20 11:52:41 +0800

    取消使用预测位置

--- NetworkGameUnity/Assets/Scripts/Module/Battle/SyncTank.cs
+++ NetworkGameUnity/Assets/Scripts/Module/Battle/SyncTank.cs
@@ -6,20 +6,20 @@ using UnityEngine;
 public class SyncTank : BaseTank
 {
     // 预测信息，哪个时间到哪个位置
-    private Vector3 lastPos;
-    private Vector3 lastRot;
-    private Vector3 forecastPos;
-    private Vector3 forecastRot;
-    private float forecastTime;
+    //private Vector3 lastPos;
+    //private Vector3 lastRot;
+    //private Vector3 forecastPos;
+    //private Vector3 forecastRot;
+    //private float forecastTime;
 
     private void Start()
     {
-        GloablMono.Instance.OnUpdate += OnUpdate;
+        //GloablMono.Instance.OnUpdate += OnUpdate;
     }
 
     private void OnUpdate(float f)
     {
-        ForecastUpdate();
+        //ForecastUpdate();
     }
 
     public override void Init(string skinPath)
@@ -29,11 +29,11 @@ public class SyncTank : BaseTank
         mRigidbody.constraints = RigidbodyConstraints.FreezeAll;
         mRigidbody.useGravity = false;
         // 初始化预测信息
-        lastPos = transform.position;
-        lastRot = transform.eulerAngles;
-        forecastPos = transform.position;
-        forecastRot = transform.eulerAngles;
-        forecastTime = Time.time;
+        //lastPos = transform.position;
+        //lastRot = transform.eulerAngles;
+        //forecastPos = transform.position;
+        //forecastRot = transform.eulerAngles;
+        //forecastTime = Time.time;
     }
 
     /// <summary>
@@ -42,16 +42,16 @@ public class SyncTank : BaseTank
     public void ForecastUpdate()
     {
         // 时间
-        float t = (Time.time - forecastTime) / CtrlTank.syncInterval;
-        t = Mathf.Clamp(t, 0, 1); // 将一个数值限制在指定的范围之内。它可以防止数值超出你设定的最小值和最大值边界
+        //float t = (Time.time - forecastTime) / CtrlTank.syncInterval;
+        //t = Mathf.Clamp(t, 0, 1); // 将一个数值限制在指定的范围之内。它可以防止数值超出你设定的最小值和最大值边界
         // 位置
         Vector3 pos = transform.position;
-        pos = Vector3.Lerp(pos, forecastPos, t);
+        //pos = Vector3.Lerp(pos, forecastPos, t);
         transform.position = pos;
         // 旋转
         Quaternion quat = transform.rotation;
-        Quaternion forecastQuat = Quaternion.Euler(forecastRot);
-        quat = Quaternion.Lerp(quat, forecastQuat, t);
+        //Quaternion forecastQuat = Quaternion.Euler(forecastRot);
+        //quat = Quaternion.Lerp(quat, forecastQuat, t);
         transform.rotation = quat;
     }
 
@@ -63,12 +63,15 @@ public class SyncTank : BaseTank
         // 预测位置
         Vector3 pos = new Vector3(msg.x, msg.y, msg.z);
         Vector3 rot = new Vector3(msg.ex, msg.ey, msg.ez);
-        forecastPos = pos + 2 * (pos - lastPos);
-        forecastRot = rot + 2 * (rot - lastRot);
-        // 更新
-        lastPos = pos;
-        lastRot = rot;
-        forecastTime = Time.time;
+        //forecastPos = pos + 2 * (pos - lastPos);
+        //forecastRot = rot + 2 * (rot - lastRot);
+        //// 更新
+        //lastPos = pos;
+        //lastRot = rot;
+        transform.position = pos;
+        transform.eulerAngles = rot;
+        Debug.Log($"Pos:{pos},Rot:{rot}");
+        //forecastTime = Time.time;
         // 炮塔
         Vector3 le = turret.localEulerAngles;
         le.y = msg.turretY;
@@ -90,6 +93,6 @@ public class SyncTank : BaseTank
 
     private void OnDestroy()
     {
-        GloablMono.Instance.OnUpdate -= OnUpdate;
+        //GloablMono.Instance.OnUpdate -= OnUpdate;
     }
 }
