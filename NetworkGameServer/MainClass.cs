63baf29153c4ee6736d7549c6399eff870344c88
BlueSky <lsxk0830@163.com>
2025-05-02 16:05:02 +0800

    更新

--- NetworkGameServer/MainClass.cs
+++ NetworkGameServer/MainClass.cs
@@ -4,19 +4,15 @@
     {
         private static void Main(string[] args)
         {
-            //if (!DbManager.Connect("networkgame", "127.0.0.1", 3306, "NetworkGame", "CYMpKLdxhLpWaHMh"))
-            if (!DbManager.Connect("game", "127.0.0.1", 3306, "root", ""))
+#if DEBUG
+            if (!DbManager.Connect("tankdb", "127.0.0.1", 3306, "root", ""))
+#else
+            if (!DbManager.Connect("tankdb", "127.0.0.1", 3306, "game_online", "LSXK0830wyyx"))
+#endif
             {
                 Console.ReadLine();
                 return;
             }
-            //if (DbManager.Register("lpy", "123456"))
-            //    Console.WriteLine("注册成功");
-            //if (DbManager.CreatePlayer("Sky"))
-            //    Console.WriteLine("创建成功");
-            //PlayerData pd = DbManager.GetPlayerData("Sky");
-            //pd.coin = 256;
-            //DbManager.UpdatePlayerData("Sky", pd);
             NetManager.StartLoop(8888);
         }
     }
