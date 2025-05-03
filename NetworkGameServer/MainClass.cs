b08da75dd846e12adf398e79923dd82e6c681aec
蓝色星空 <lsxk0830@163.com>
2025-05-03 10:28:58 +0800

    更新UI

--- NetworkGameServer/MainClass.cs
+++ NetworkGameServer/MainClass.cs
@@ -5,7 +5,7 @@
         private static void Main(string[] args)
         {
 #if DEBUG
-            if (!DbManager.Connect("tankdb", "127.0.0.1", 3306, "root", ""))
+            if (!DbManager.Connect("game", "127.0.0.1", 3306, "root", ""))
 #else
             if (!DbManager.Connect("tankdb", "127.0.0.1", 3306, "game_online", "LSXK0830wyyx"))
 #endif
