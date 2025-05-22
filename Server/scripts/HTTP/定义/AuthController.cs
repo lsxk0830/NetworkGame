db8efbfde78541c5c0e01468161c6c6b7735f9e6
蓝色星空 <lsxk0830@163.com>
2025-05-22 18:10:15 +0800

    增加上传头像API

--- Server/scripts/HTTP/定义/AuthController.cs
+++ Server/scripts/HTTP/定义/AuthController.cs
@@ -27,4 +27,14 @@ public static partial class AuthController
         await context.Response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
         context.Response.OutputStream.Close();
     }
+
+    /// <summary>
+    /// 获取时间戳
+    /// </summary>
+    /// <returns></returns>
+    public static long GetTimeStamp()
+    {
+        TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        return Convert.ToInt64(ts.TotalSeconds);
+    }
 }
