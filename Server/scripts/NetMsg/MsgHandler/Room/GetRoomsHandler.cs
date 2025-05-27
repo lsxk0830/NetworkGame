743aff7227485fc4246fdd53c5a33796af4f1b21
BlueSky <lsxk0830@163.com>
2025-05-27 11:41:22 +0800

    更新房间面板，取消开发模式生成面板

--- Server/scripts/NetMsg/MsgHandler/Room/GetRoomsHandler.cs
+++ Server/scripts/NetMsg/MsgHandler/Room/GetRoomsHandler.cs
@@ -7,11 +7,6 @@
     {
         Console.WriteLine($"接收:MsgGetRooms协议");
 
-#if DEBUG
-        Room room1 = RoomManager.CreateRoom();
-        Room room2 = RoomManager.CreateRoom();
-#endif
-
         User? user = cs.user;
         if (user == null) return;
         NetManager.Send(cs, RoomManager.SendRoomsToMsg());
