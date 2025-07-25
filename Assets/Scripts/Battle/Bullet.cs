52a967cb54b23cab4b1d0bdcf7932b129febfcf1
蓝色星空 <lsxk0830@163.com>
2025-07-25 16:09:54 +0800

    优化子弹逻辑

--- Assets/Scripts/Battle/Bullet.cs
+++ Assets/Scripts/Battle/Bullet.cs
@@ -5,12 +5,14 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 50f; // 子弹移动速度
     public float maxDistance = 200f;
+    private float dis = 0.1f;// 只在移动距离足够时检测碰撞
     public LayerMask hitLayers; // 可命中的层级（如 Enemy | Friend | Bullet | CanDestroy）
     public Guid bulletID;
     public long ID; // 发射者ID
 
     private Vector3 startPos; // 初始位置
     private Vector3 lastPos;
+    private bool isMove; // 子弹是否移动
 
     public AudioSource audioSource;
 
@@ -26,6 +28,8 @@ public class Bullet : MonoBehaviour
         bulletID = bulletGuid;
         transform.position = startPos;
         this.startPos = startPos; // 保存初始位置
+        lastPos = startPos;
+        isMove = true;
 
         if (BattleManager.EffectActive)
         {
@@ -46,53 +50,61 @@ public class Bullet : MonoBehaviour
 
     private void OnFixedUpdate()
     {
-        if (ID != GameMain.ID) return;
+        if (ID != GameMain.ID || !isMove) return;
 
-        // 检测两帧之间的命中
-        if (Physics.Linecast(lastPos, transform.position, out RaycastHit hit, hitLayers))
+        // 只在移动距离足够时检测碰撞
+        if ((transform.position - lastPos).sqrMagnitude > dis * dis)
         {
-            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+            // 检测两帧之间的命中
+            if (Physics.Linecast(lastPos, transform.position, out RaycastHit hit, hitLayers))
             {
-                if (hit.collider.TryGetComponent(out SyncTank hitTank))
+                isMove = false;// 标记子弹停止移动
+                if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Enemy") &&
+                    hit.collider.TryGetComponent(out SyncTank hitTank))
                 {
                     MsgHit msgHit = this.GetObjInstance<MsgHit>();
                     msgHit.targetID = hitTank.ID; // 被击中坦克ID
                     msgHit.ID = ID; // 发射者ID
-                    msgHit.x = transform.position.x.RoundTo(4);
-                    msgHit.y = transform.position.y.RoundTo(4);
-                    msgHit.z = transform.position.z.RoundTo(4);
+                    msgHit.x = hit.point.x.RoundTo(4);
+                    msgHit.y = hit.point.y.RoundTo(4);
+                    msgHit.z = hit.point.z.RoundTo(4);
                     NetManager.Instance.Send(msgHit);
                     this.PushPool(msgHit);
                     Debug.Log($"发送击中受伤协议)");
                 }
+                MsgFire msg = this.GetObjInstance<MsgFire>();
+                msg.ID = ID; // 发射者ID
+                msg.bulletID = bulletID; // 子弹ID
+                msg.x = hit.point.x.RoundTo(4);
+                msg.y = hit.point.y.RoundTo(4);
+                msg.z = hit.point.z.RoundTo(4);
+                msg.IsExplosion = true;
+                NetManager.Instance.Send(msg);
+                this.PushPool(msg); // 将消息对象归还对象池
+
+                this.PushGameObject(this.gameObject); // 将子弹归还对象池
+                this.GetGameObject(EffectManager.HitPrefab)
+                    .GetComponent<Hit>()
+                    .PoolInit(hit.point);
             }
-            MsgFire msg = this.GetObjInstance<MsgFire>();
-            msg.ID = ID; // 发射者ID
-            msg.bulletID = bulletID; // 子弹ID
-            msg.x = transform.position.x.RoundTo(4);
-            msg.y = transform.position.y.RoundTo(4);
-            msg.z = transform.position.z.RoundTo(4);
-            msg.IsExplosion = true;
-            NetManager.Instance.Send(msg);
-            this.PushPool(msg); // 将消息对象归还对象池
-
-            this.PushGameObject(this.gameObject); // 将子弹归还对象池
-            this.GetGameObject(EffectManager.HitPrefab)
-                .GetComponent<Hit>()
-                .PoolInit(this.transform.position);
+            lastPos = transform.position;
         }
-        lastPos = transform.position;
     }
-
 
     private void OnUpdate()
     {
+        if (!isMove) return;
+
         // 移动子弹
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // 超出射程销毁
         if ((transform.position - startPos).sqrMagnitude > maxDistance * maxDistance)
         {
+            isMove = false;
+            this.GetGameObject(EffectManager.HitPrefab)
+                .GetComponent<Hit>()
+                .PoolInit(this.transform.position);
             this.PushGameObject(gameObject);
         }
     }
